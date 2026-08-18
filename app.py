53aae582b289fd332542590f3739f3eb32c74749
Samyuktha Thota <thotasamyuktha952@gmail.com>
2026-08-18 20:51:22 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -1034,9 +1034,10 @@ def job_details(job_id):
 # ==========================================
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
 
     app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000
+        debug=False,
+        host="0.0.0.0",
+        port=port
     )
